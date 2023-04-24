9ecd6b72405c9fcafc59b5c4e222828da1d01a5d
chaimaeelfellioui <chaimae1307@gmail.com>
2023-04-24 08:27:51 -0700

    functions1.c

--- functions1.c
+++ functions1.c
@@ -76,6 +76,7 @@ int print_octal(va_list types, char buffer[],
 	
 	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
+
 /************** PRINT UNSIGNED NUMBER IN HEXADECIMAL **************/
 /**
  * print_hexadecimal - Prints an unsigned number in hexadecimal notation
